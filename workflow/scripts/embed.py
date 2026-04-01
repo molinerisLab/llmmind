594797a71f90cbd71860181512785b024ddcbb43
Ivan Molineris <ivan.molineris@unito.it>
2026-04-01 12:05:28 +0200

    Initial commit

---
+++ workflow/scripts/embed.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from models import load_llm
+
+language_model = load_llm(llm_model_name)
---
+++ workflow/scripts/save_pretrained.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys  
+import os
+from transformers import AutoModel, AutoTokenizer
+
+def save_pretrained_model(model_name, save_dir):
+    model = AutoModel.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model.save_pretrained(save_dir)
+    tokenizer.save_pretrained(save_dir)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python save_pretrained.py <model_name> <save_dir>")
+        sys.exit(1)
+    model_name = sys.argv[1]
+    save_dir = sys.argv[2]
+    save_pretrained_model(model_name, save_dir)
