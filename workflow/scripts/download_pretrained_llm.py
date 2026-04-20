ec01e9358121ecd5322fb0dd9f96d3ea5517d534
Jonas Salvalaggio <jonas.salvalaggio@unito.it>
2026-04-20 20:07:50 +0200

    small cleanup; modified download_pretrained_llm.py to avoid saving models on RAM during download

--- workflow/scripts/download_pretrained_llm.py
+++ workflow/scripts/download_pretrained_llm.py
@@ -1,16 +1,18 @@
 import sys
-from transformers import AutoModel, AutoTokenizer
+from huggingface_hub import snapshot_download
 
-def save_pretrained_model(model_name, save_dir):
-    model = AutoModel.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model.save_pretrained(save_dir)
-    tokenizer.save_pretrained(save_dir)
+def download_model_repo(model_name: str, save_dir: str) -> None:
+    snapshot_download(
+        repo_id=model_name,
+        local_dir=save_dir,
+        local_dir_use_symlinks=False,
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python3 workflow/scripts/download_pretrained_llm.py <model_name> <save_dir>")
+        print("Usage: python3 download_pretrained_llm.py <model_name> <save_dir>")
         sys.exit(1)
+
     model_name = sys.argv[1]
     save_dir = sys.argv[2]
-    save_pretrained_model(model_name, save_dir)
+    download_model_repo(model_name, save_dir)
